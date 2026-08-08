5051ffecd1dbf8cda84f42a42a6869c20e595724
JMater1199 <Syafunkojoshua@gmail.com>
2026-08-08 12:14:47 +0000

    lol save pls

--- Phone store.py
+++ Phone store.py
@@ -2,92 +2,62 @@ import random
 
 laptop = {'brand':'Dell', 'Model': 'ABC111', 'Processor': 'Intel Core i5'}
 
-# , 'Speed': 3.15, 'RAM': 16, 'HardDisk': 512, 'Screen': 14.9}
 
-
-#laptop = {'brand':'Dell', 'Model': 'ABC111', 'Processor': 'Intel i5', 'Speed': 3.15, 'RAM': 16, 'HardDisk': 512, 'Screen': 14.9}
-
-print('Welcome to WiByte Laptop store.')
+print('Welcome to joshua\s Laptop store.')
 print('We have a huge inventory of laptops.')
 print('Please indicate your preference of specs.')
 print()
 
-
 def append_s(name):
   return name + 's'
 
-
-# Display the laptop configuration
-
 print()
 
-
-
-# Create a list of laptops
-laptops_list      = []
-#brands_list       = ['Dell', 'Asus', 'Acer', 'Apple']
-#CPU_list          = ['Intel i5', 'Intel i7', 'AMD', 'Apple']
-#Speed_list        = [2, 3.15, 3.8]
-#RAM_list          = [4, 8, 16]
-#HardDisk_list     = [256, 512, 1024]
-#ScreenSize_list   = [12, 14.9, 17]
+l_list      = []
 
 specs = ['Brand', 'Model', 'CPU', 'Speed', 'RAM', 'Storage', 'Screensize', 'Price']
 
-specs_new = list(map(append_s, specs))
-master_dict = dict.fromkeys(specs_new)
+s_new = list(map(append_s, specs))
+m_dict = dict.fromkeys(s_new)
 
-master_dict['Brands'] =  ['WDell', 'WAsus', 'WAcer', 'WLenovo', 'WHP']
-master_dict['Models'] = ['AAA', 'BBB', 'CCC']
-master_dict['CPUs'] = ['WIntel i5', 'WIntel i7', 'WAMD Ryzen']
-master_dict['Speeds'] = ['2 GHz', '3 GHz', '3.15 GHz', '3.8 GHz']
-master_dict['RAMs'] = [ '2 GB', '4 GB', '8 GB', '16 GB']
-master_dict['Storages'] = [ '128 GB', '256 GB', '512 GB', '1024 GB']
-master_dict['Screensizes'] = [ '9 in', '12 in', '14.9 in', '17 in']
-master_dict['Prices'] = ['INR 20000', 'INR 30000', 'INR 40000']
-
+m_dict['Brands'] =  ['WDell', 'WAsus', 'WAcer', 'WLenovo', 'WHP']
+m_dict['Models'] = ['AAA', 'BBB', 'CCC']
+m_dict['CPUs'] = ['WIntel i5', 'WIntel i7', 'WAMD Ryzen']
+m_dict['Speeds'] = ['2 GHz', '3 GHz', '3.15 GHz', '3.8 GHz']
+m_dict['RAMs'] = [ '2 GB', '4 GB', '8 GB', '16 GB']
+m_dict['Storages'] = [ '128 GB', '256 GB', '512 GB', '1024 GB']
+m_dict['Screensizes'] = [ '9 in', '12 in', '14.9 in', '17 in']
+m_dict['Prices'] = ['INR 20000', 'INR 30000', 'INR 40000']
 
 for n_laptops in range(60):
-  new_laptop = dict.fromkeys(specs)
-  for kk in new_laptop:
-    new_laptop[kk] = random.choice(master_dict[kk+'s'])
-  laptops_list.append(new_laptop)  
+  n_laptop = dict.fromkeys(specs)
+  for kk in n_laptop:
+    n_laptop[kk] = random.choice(m_dict[kk+'s'])
+  l_list.append(n_laptop)  
   
-# Seek user preference
-
-# Display laptops that meet the criterion
-
-#print(kk)
-user_choice = dict.fromkeys(specs)
+u_choice = dict.fromkeys(specs)
 for kk in specs:
-  user_choice[kk] = input('Any preference for ' + kk + ' (Enter none for no preference)'+'\n')
+  u_choice[kk] = input('Any preference for ' + kk + ' (Enter none for no preference)'+'\n')
   
-# Now create the query
-
 query = ''
 
-for kk in user_choice:
-  if user_choice[kk].lower() == 'none':
+for kk in u_choice:
+  if u_choice[kk].lower() == 'none':
     pass
   else:
-    query = query + 'laptop[' + '\'' + kk + '\'] == ' + '\'' + user_choice[kk] + '\' and '
+    query = query + 'laptop[' + '\'' + kk + '\'] == ' + '\'' + u_choice[kk] + '\' and '
 
 print(query)
-
 
 input()
 
 query = query[0:-4:1]
 
-# Print top row
-
 if query != '':
-  selected = [laptop for laptop in laptops_list if eval(query)]
+  selected = [laptop for laptop in l_list if eval(query)]
 else:
-  selected = [laptop for laptop in laptops_list]
+  selected = [laptop for laptop in l_list]
   
-# Print top row (all the keys of specs)
-
 print(len(selected), 'laptops met your preference.')
 
 characters = 0
@@ -106,6 +76,7 @@ for laptop in selected:
     print((12 - characters)*' ', end = '')
   print()
   
+  
 
 '''
 
@@ -114,37 +85,37 @@ for kk in laptop:
 
 print()
 
-for jj in range(len(laptops_list)):
+for jj in range(len(l_list)):
   for kk in laptop:
     if kk in units:
-      print(str(laptops_list[jj][kk]) + ' ' + units[kk], end='\t\t')
+      print(str(l_list[jj][kk]) + ' ' + units[kk], end='\t\t')
     else:
-      print(str(laptops_list[jj][kk]), end='\t\t')
+      print(str(l_list[jj][kk]), end='\t\t')
   print()
 
 
-user_choice_laptop = dict.fromkeys(keys_list)
+u_c_l = dict.fromkeys(keys_list)
 
-user_choice_brand = input('Your preferred brand (Acer/Asus/HP/Dell/Any')
+u_c_b = input('Your preferred brand (Acer/Asus/HP/Dell/Any')
 
-user_choice_laptop['brand'] = user_choice_brand
+u_c_l['brand'] = u_c_b
 
-user_choice_CPU = input('Your preferred CPU (Intel i5/Intel i7/AMD/Any')
+u_c_C = input('Your preferred CPU (Intel i5/Intel i7/AMD/Any')
 
-user_choice_laptop['CPU'] = user_choice_CPU
+u_c_l['CPU'] = user_choice_CPU
 
 for kk in laptop:
   print(kk, end = '\t\t')
 
 print()
 
-for jj in range(len(laptops_list)):
+for jj in range(len(l_list)):
   for kk in laptop:
-    if user_choice_laptop[kk] == laptops_list[jj][kk] or user_choice_laptop[kk] == 'Any':
+    if u_c_l[kk] == l_list[jj][kk] or u_c_l[kk] == 'Any':
       if kk in units:
-         print(str(laptops_list[jj][kk]) + ' ' + units[kk], end='\t\t')
+         print(str(l_list[jj][kk]) + ' ' + units[kk], end='\t\t')
       else:
-        print(str(laptops_list[jj][kk]), end='\t\t')
+        print(str(l_list[jj][kk]), end='\t\t')
     else:
       break
   print()
